9ad7fe47ddc40a0098cede520f47a6a906b56fde
MehmetSB <31502193+MehmetSB@users.noreply.github.com>
2018-04-14 02:46:26 +0200

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -11,40 +11,41 @@ var HasRest = false;
 //Startpage
 opt1.style.visibility='hidden';
 opt3.style.visibility='hidden';
+// SET lvl.style.visibility op hidden als je klaar bent. AAAAAAAAAAAAAAAA
 lvl.style.visibility='hidden';
 opt2.onclick = function(){
-	Level1();
+	mountain();
 	opt1.style.visibility='visible';
 	opt3.style.visibility='visible';
 
 }
 
-function Level1() {
+function mountain() {
 	console.log("Mountain()");
-	lvl.innerHTML = 'Start';
+	lvl.innerHTML = 'Mountain';
 	img.src = 'img/building.jpg';
 	h1.innerHTML = "You are now on the Great Plateau";
-	opt1.setAttribute("onClick", "Level2();");
+	opt1.setAttribute("onClick", "woods();");
 	opt1.innerHTML = 'Go to the woods';
 	// only allow option 2 if user has a key in his inventory
 	opt2.innerHTML = 'Go to the building';
 	opt2.onclick = function(){
 		if(HasKey){
-			Level3();}
+			temple();}
 			else {
 				alert('You need a key to enter this building.');
 			}
 		}
 		opt3.innerHTML ='Climb the mountain';
-		opt3.setAttribute("onClick", "Level4();");
+		opt3.setAttribute("onClick", "mountain2();");
 	}
 
-function Level2() {
+function woods() {
 	console.log("LostWoods()");
-	lvl.innerHTML = 'Level 2';
+	lvl.innerHTML = 'Woods';
 	img.src = 'img/2ways.jpg';
 	h1.innerHTML = "You are now in the Lost Woods, There could be items hidden in the chests.";
-	opt1.setAttribute("onClick", "Level10();");
+	opt1.setAttribute("onClick", "temple2();");
 	opt1.innerHTML = 'Go Left';
 	//option 2 puts a key in the user's inventory.
 	opt2.innerHTML = 'Search the chests';
@@ -53,16 +54,16 @@ function Level2() {
 		alert('You found a Key!');
 	}
 	opt3.innerHTML = 'Go Right';
-	opt3.setAttribute("onClick", "Level1();");
+	opt3.setAttribute("onClick", "mountain();");
 }
-function Level3() {
-	console.log("TempeofTime()");
-	lvl.innerHTML = 'Level 3';
+function temple() {
+	console.log("TempleofTime()");
+	lvl.innerHTML = 'Temple of Time';
 	img.src = 'img/sword.jpg';
 	h1.innerHTML = "You are now in the Temple of Time.";
 	opt1.innerHTML = 'Leave the building';
 	opt1.onclick = function(){
-		Level1();
+		mountain();
 		opt2.style.visibility='visible';
 		}
 		opt2.style.visibility='hidden';
@@ -70,16 +71,16 @@ function Level3() {
 		opt3.innerHTML = 'Take the Sword';
 		opt3.onclick = function(){
 			HasSword = true;
-			alert('You recovered your memories!');
+			alert('You found a sword!');
 		}
 	}
 
-function Level4() {
+function mountain2() {
 	console.log("TopMountain()");
-	lvl.innerHTML = 'Level 4';
+	lvl.innerHTML = 'Top of the mountain';
 	img.src = 'img/mountain.jpg';
 	h1.innerHTML = "You are now on the top of the Great Plateau";
-	opt1.setAttribute("onClick", "Level1();");
+	opt1.setAttribute("onClick", "mountain();");
 	opt1.innerHTML = 'Descend the mountain';
 	//option 2 gives an alert/pop-up to the user.
 	opt2.innerHTML = 'Jump';
@@ -87,13 +88,13 @@ function Level4() {
 		alert('You just killed yourself.');
 		location.reload();
 	}
-	opt3.setAttribute("onClick", "Level5();");
+	opt3.setAttribute("onClick", "battle();");
 	opt3.innerHTML = 'Use a paraglider';
 }
 
-function Level5() {
+function battle() {
 	console.log("Battle()");
-	lvl.innerHTML = 'Level 5';
+	lvl.innerHTML = 'Battle';
 	img.src = 'img/fight.jpg';
 	h1.innerHTML = "You have been attacked by a monster, try to fight or run!";
 	// only allow option 1 if user has a sword in his inventory
@@ -102,14 +103,15 @@ function Level5() {
 	opt3.innerHTML ='Run';
 	opt1.onclick = function(){
 		if(HasSword){
-			Level6();
+			ending();
+				opt2.style.visibility='visible';
 		}
 		else {
-			alert('You have no weapon so you died.');
+			alert('You had no weapon so you died.');
 			location.reload();
 		}
 		if(HasRest){
-		Level6();
+		ending();
 }
 	else {
 		alert('You lost the battle because you had no energy left.')
@@ -117,24 +119,15 @@ function Level5() {
 	}
 	}
 	opt3.onclick = function(){
-		Level9();
+		safehouse();
 		opt2.style.visibility='visible';
 	}
 }
 
-function Level6() {
-	console.log("Victory()");
-	lvl.innerHTML = 'Level 5';
-	img.src = 'img/victory.jpg';
-	h1.innerHTML = "You have finished the game";
-	opt1.style.visibility='hidden';
-	opt2.style.visibility='hidden';
-	opt3.style.visibility='hidden';
-}
 
-function Level10() {
+function temple2() {
 	console.log("ForestTemple()");
-	lvl.innerHTML = 'Level 10';
+	lvl.innerHTML = 'Forest Temple';
 	img.src = 'img/forest-temple.jpg';
 	h1.innerHTML = "This is the Forest of the Dead Temple, be careful no one ever made it out of here.";
 	// only allow option 1 if user has a sword in his inventory
@@ -142,15 +135,15 @@ function Level10() {
 	opt2.style.visibility='hidden';
 	opt3.innerHTML ='Go back';
 	opt1.onclick = function(){
-		Level7();
+		defeat();
 	}
 	opt3.onclick = function(){
-		Level2();
+		woods();
 		opt2.style.visibility='visible';
 	}
 }
 
-function Level7() {
+function defeat() {
 	console.log("GameOver()");
 	lvl.innerHTML = 'Game Over';
 	img.src = 'img/dead.jpg';
@@ -164,7 +157,7 @@ function Level7() {
 	}
 }
 
-function Level9() {
+function safehouse() {
 	console.log("Safehouse()");
 	lvl.innerHTML = 'You found a safehouse!';
 	img.src = 'img/rest.jpg';
@@ -173,7 +166,7 @@ function Level9() {
 	opt1.innerHTML = 'Travel West';
 	opt1.onclick = function(){
 		if(HasRest){
-			Level5();
+			battle();
 		} else {
 			alert('You look tired, you need to take a rest');
 		}
@@ -186,9 +179,65 @@ function Level9() {
 	opt3.innerHTML ='Travel East';
 	opt3.onclick = function(){
 		if(HasRest){
-			Level5();
+			battle2();
 		} else {
 			alert('You look tired, you need to take a rest');
 		}
 	}
 }
+
+function battle2() {
+	console.log("Battle2()");
+	lvl.innerHTML = 'Battle 2';
+	img.src = 'img/fight2.jpg';
+	h1.innerHTML = "You have been attacked by a monster, try to fight or run!";
+	opt1.innerHTML = 'Fight';
+	opt2.style.visibility='hidden';
+	opt3.innerHTML = 'Run';
+	opt1.onclick = function(){
+		if(HasSword){
+		ending2();
+		opt2.style.visibility='visible';
+	}
+	else {
+		alert('You had no weapon so you died.');
+		location.reload();
+	}
+	if(HasRest){
+		ending2();
+	}
+	else {
+		alert('You lost the battle because you had no energy left.');
+	}
+	opt3.onclick = function(){
+		safehouse();
+		opt2.style.visibility='visible';
+	}
+}
+}
+
+function ending() {
+	console.log("Ending()");
+	lvl.innerHTML = 'Ending';
+	img.src = 'img/ending.jpg';
+	h1.innerHTML = "You beated the enemy and brought the peace back to the world.";
+	opt1.style.visibility='hidden';
+	opt2.innerHTML = "Restart"
+	opt2.onclick = function(){
+		location.reload();
+	}
+	opt3.style.visibility='hidden';
+}
+
+function ending2() {
+	console.log("Ending2()");
+	lvl.innerHTML = 'Ending';
+	img.src = 'img/ending2.jpg';
+	h1.innerHTML = "You beated the enemy, but you absorbed his dark power and brought darkness to the world. Play again for an alternate ending.";
+	opt1.style.visibility='hidden';
+	opt2.innerHTML = "Restart"
+	opt2.onclick = function(){
+		location.reload();
+	}
+	opt3.style.visibility='hidden';
+	}
